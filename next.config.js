affbdf0d309fad928601cdf733b99577ffdeadf4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-05 00:23:47 +0900

    site

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  unstable_stork: true,
+  unstable_staticImage: true,
 })
 module.exports = withNextra(nextConfig)
