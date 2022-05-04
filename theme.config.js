309beceae0e1e623c140cdfd887f311fc632648a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-05 00:24:17 +0900

    사이트 설정 관련 추가 작업 (#5)

--- theme.config.js
+++ theme.config.js
@@ -2,18 +2,19 @@
 export default {
   projectLink: 'https://github.com/kdy1/devmap',
   docsRepositoryBase: 'https://github.com/kdy1/devmap/blob/main',
-  titleSuffix: ' – Dev Map',
-  nextLinks: true,
-  prevLinks: true,
+  titleSuffix: ' - Dev Map',
+  nextLinks: false,
+  prevLinks: false,
   search: true,
   customSearch: null, // customizable, you can use algolia for example
+  unstable_stork: true,
+  floatTOC: true,
   darkMode: true,
   footer: true,
   footerText: `MIT ${new Date().getFullYear()} © kdy1.`,
   footerEditLink: `Edit this page on GitHub`,
   logo: (
     <>
-      <svg>...</svg>
       <span>개발자 로드맵 </span>
     </>
   ),
