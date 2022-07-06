47c3ad9c039287b762b6aee7de85af67a2d44a12
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-06 21:05:46 +0900

    git 설명 (#4)

--- next.config.js
+++ next.config.js
@@ -6,7 +6,5 @@ const nextConfig = {
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_stork: true,
-  unstable_staticImage: true,
 })
 module.exports = withNextra(nextConfig)
